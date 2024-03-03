5631fa397d666ee0d5109ff943e45f671e300a80
aletardio <ale.tardio99@gmail.com>
2024-03-03 20:59:52 +0100

    Added img placeholder

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -7,6 +7,8 @@
                 <h3 class="text-uppercase">{{ $project->title }}</h3>
                 @if ($project->cover_image)
                     <img src="{{ asset('/storage/' . $project->cover_image) }}" alt="{{ $project->title }}" class="w-50">
+                @else
+                    <img src="{{ asset('/img/placeholder.webp') }}" alt="{{ $project->title }}" class="w-50">
                 @endif
                 <h5 class="my-2">
                     <strong>{{ !empty($project->type->name) ? $project->type->name : 'Senza tipo' }}</strong>
